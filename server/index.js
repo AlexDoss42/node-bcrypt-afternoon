376c30858cf0919bbcd2fa84984d71eeb18cabe7
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-04-25 15:30:39 -0600

    Step 2 part 1(backend)

--- server/index.js
+++ server/index.js
@@ -2,25 +2,27 @@ require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
 const session = require('express-session')
+const authCtrl = require('./controllers/authController')
 
 const port = 4000
 
-const {CONNECT_STRING, SESSION_SECRET} = process.env
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
 
 const app = express()
 
 app.use(express.json())
+
+massive(CONNECTION_STRING).then(db => {
+  app.set('db', db)
+  console.log(`It's working! IT'S WORKING!!!!`)
+}).catch(() => console.log('It is broke at the massive invocation'))
+
 app.use(session({
   secret: SESSION_SECRET,
   resave: true,
   saveUninitialized: false
 }))
 
-massive(CONNECT_STRING).then(db => {
-  app.set('db', db)
-  console.log(`It's working! IT'S WORKING!!!!`)
-})
+app.post('/auth/register', authCtrl.register)
 
-app.listen(port, () => {
-  console.log(`It's over Anakin. I have the ${port} port`)
-})
+app.listen(port, () => console.log(`It's over Anakin. I have the ${port} port`))
